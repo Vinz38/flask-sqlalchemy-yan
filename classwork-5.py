4258f9a74a83aad1426b5321b0ff51f3fc62baf9
Vinz38 <ilia.nikulin09@gmail.com>
2026-03-15 18:35:17 +0300

    5 задача из классных

---
+++ classwork-5.py
@@ -0,0 +1,13 @@
+from data import db_session
+from data.users import User
+
+
+db_name = input()
+
+db_session.global_init(f"db/{db_name}")
+db_sess = db_session.create_session()
+
+users = db_sess.query(User).filter(User.address == "module_1").all()
+
+for user in users:
+    print(f"<Colonist> {user.id} {user.surname} {user.name}")
