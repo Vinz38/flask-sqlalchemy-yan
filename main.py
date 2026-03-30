3be79489bdd955e2db51e8fc07b91b21ae687247
VinzJDH <ilia.nikulin09@gmail.com>
2026-03-30 15:07:23 +0300

    небольшие изменения для 5 задачи

--- main.py
+++ main.py
@@ -17,6 +17,9 @@ app = Flask(__name__)
 api = Api(app)
 api.add_resource(users_resource.JobsListResource, '/api/v2/jobs') 
 api.add_resource(users_resource.JobsResource, '/api/v2/news/<int:jobs_id>')
+api.add_resource(users_resource.JobsListResource, '/api/v2/users') 
+api.add_resource(users_resource.JobsResource, '/api/v2/news/<int:users_id>')
+
 
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 app.register_blueprint(auth_bp)
