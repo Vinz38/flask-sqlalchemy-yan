94c8a0923ff411bb8ae518584fbbb3fd65052804
VinzJDH <ilia.nikulin09@gmail.com>
2026-03-30 13:46:06 +0300

    Небольшие правки 1 задачи RESTful-API

--- data/users_resource.py
+++ data/users_resource.py
@@ -4,53 +4,55 @@ from .jobs import Jobs
 from flask import jsonify
 
 
-def abort_if_news_not_found(news_id):
+def abort_if_jobs_not_found(jobs_id):
     session = db_session.create_session()
-    news = session.query(Jobs).get(news_id)
-    if not news:
-        abort(404, message=f"News {news_id} not found")
+    jobs = session.query(Jobs).get(jobs_id)
+    if not jobs:
+        abort(404, message=f"Jobs {jobs_id} not found")
 
 
 class JobsResource(Resource):
-    def get(self, news_id):
-        abort_if_news_not_found(news_id)
+    def get(self, jobs_id):
+        abort_if_jobs_not_found(jobs_id)
         session = db_session.create_session()
-        news = session.get(Jobs, news_id)
-        return jsonify({'news': news.to_dict(
-            only=('title', 'content', 'user_id', 'is_private'))})
+        jobs = session.get(Jobs, jobs_id)
+        return jsonify({'jobs': jobs.to_dict(
+            only=('team_leader', 'job', 'work_size', 'collaborators', 'is_finished'))})
 
-    def delete(self, news_id):
-        abort_if_news_not_found(news_id)
+    def delete(self, jobs_id):
+        abort_if_jobs_not_found(jobs_id)
         session = db_session.create_session()
-        news = session.get(Jobs, news_id)
-        session.delete(news)
+        jobs = session.get(Jobs, jobs_id)
+        session.delete(jobs)
         session.commit()
         return jsonify({'success': 'OK'})
 
 
 parser = reqparse.RequestParser()
-parser.add_argument('title', required=True)
-parser.add_argument('content', required=True)
-parser.add_argument('is_private', required=True, type=bool)
-parser.add_argument('user_id', required=True, type=int)
+parser.add_argument('team_leader', required=True, type=int)
+parser.add_argument('job', required=True)
+parser.add_argument('work_size', required=True, type=int)
+parser.add_argument('collaborators', required=True)
+parser.add_argument('is_finished', required=True, type=bool)
 
 
 class JobsListResource(Resource):
     def get(self):
         session = db_session.create_session()
-        news = session.query(Jobs).all()
-        return jsonify({'news': [item.to_dict(
-            only=('title', 'content', 'user.name')) for item in news]})
+        jobs = session.query(Jobs).all()
+        return jsonify({'jobs': [item.to_dict(
+            only=('team_leader', 'job', 'work_size', 'collaborators', 'is_finished')) for item in jobs]})
 
     def post(self):
         args = parser.parse_args()
         session = db_session.create_session()
-        news = Jobs(
-            title=args['title'],
-            content=args['content'],
-            user_id=args['user_id'],
-            is_private=args['is_private']
+        jobs = Jobs(
+            team_leader=args['team_leader'],
+            job=args['job'],
+            work_size=args['work_size'],
+            collaborators=args['collaborators'],
+            is_finished=args['is_finished']
         )
-        session.add(news)
+        session.add(jobs)
         session.commit()
-        return jsonify({'id': news.id})
+        return jsonify({'id': jobs.id})
